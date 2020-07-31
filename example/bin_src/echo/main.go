ff6ab23a3ce6fdaab27a1e4c0adb9953dd974913
Amadeus_vn <git@amas.dev>
2020-08-01 02:09:33 +0900

    fix data Marshalize mistake

--- example/bin_src/echo/main.go
+++ example/bin_src/echo/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"io/ioutil"
 	"os"
 
@@ -9,42 +8,40 @@ import (
 	pb "github.com/gw31415/bots/proto"
 )
 
-var stdin = bufio.NewScanner(os.Stdin)
-
 func main() {
-	
+
 	//標準入力から全てバイト列で読み込む
 	arg, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic(err)
 	}
-	
+
 	//バイト列をデシリアライズして扱えるようにする
 	in_pb := &pb.Input{}
 	if err := proto.Unmarshal(arg, in_pb); err != nil {
 		panic(err)
 	}
-	
+
 	//出力データをまとめる
 	out_pb := &pb.Output{
 		Msgs: []*pb.BotMsg{
 			{
 				Medias: []*pb.OutputMedia{
 					{
-						Data: arg,
+						Data: in_pb.Media[0].Data,
 						Type: pb.OutputMedia_UTF8,
 					},
 				},
 			},
 		},
 	}
-	
+
 	//シリアライズしてバイト列にする
 	out, err := proto.Marshal(out_pb)
 	if err != nil {
 		panic(err)
 	}
-	
+
 	//出力
 	os.Stdout.Write(out)
 
