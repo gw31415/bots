4b481ce388c59e4721f1702f454b5fe696f52b0f
Amadeus_vn <git@amas.dev>
2020-08-06 06:10:04 +0900

    fix sym

--- examples/bin_src/sym/main.py
+++ examples/bin_src/sym/main.py
@@ -2,13 +2,14 @@
 from sys import stdin, stdout, argv, exit
 from proto import cmdin_pb2, cmdout_pb2, help_pb2
 from sympy import latex, parse_expr
-if argv[1] == "--help" or argv[1] == "-h":
-    hp = help_pb2.Help()
-    hp.usage = "[expr]"
-    hp.short_description = "Parse expr of sympy"
-    hp.long_description = "Parse and simplify expr of sympy"
-    stdout.buffer.write(hp.SerializeToString())
-    exit(0)
+if len(argv) > 1:
+    if argv[1] == "--help" or argv[1] == "-h":
+        hp = help_pb2.Help()
+        hp.usage = "[expr]"
+        hp.short_description = "Parse expr of sympy"
+        hp.long_description = "Parse and simplify expr of sympy"
+        stdout.buffer.write(hp.SerializeToString())
+        exit(0)
 stdin = stdin.buffer.read()
 in_pb = cmdin_pb2.Input()
 in_pb.ParseFromString(stdin)
